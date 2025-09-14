0396d1b4ddfaa583c4d56380b07f2efe87902dbd
ink616 <taylormedolphe@gmail.com>
2025-09-14 00:15:50 -0300

    Add files via upload

---
+++ LEVEL-UP/JS/admin.js
@@ -0,0 +1,17 @@
+function btn_crearUsuario() {
+    window.location.href="crearUsuario.html"
+}
+function btn_crearProducto() {
+    window.location.href="crearProducto.html"
+}
+function btn_modificarUsuario() {
+    window.location.href="modificarUsuario.html"
+}function btn_modificarProducto() {
+window.location.href="modificarProducto.html"
+}
+function btn_mostrarUsuario() {
+window.location.href="mostrarUsuario.html"
+}
+function btn_mostrarProducto() {
+window.location.href="mostrarProducto.html"
+}
